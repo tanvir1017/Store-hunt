61817f79582cd30fb1098931a3a6e46ddbbe6892
Tanvir Hossain <hossainmdjabed037@gmail.com>
2021-12-18 21:42:02 +0600

    firebase

--- src/Components/Authentication/Firease/Firebase.config.js
+++ src/Components/Authentication/Firease/Firebase.config.js
@@ -8,3 +8,10 @@ const firebaseConfig = {
 };
 
 export default firebaseConfig;
+/* 
+REACT_APP_API_KEY=AIzaSyDRaR5RSf_WjOlZoQuQCMpeiQLGR1s7dds
+REACT_APP_AUTH_DOMAIN=store-hunt.firebaseapp.com
+REACT_APP_PROJECT_ID=store-hunt
+REACT_APP_STORAGE_BUCKET=store-hunt.appspot.com
+REACT_APP_MESSAGING_SENDER_ID=698458466607
+REACT_APP_APP_ID=1:698458466607:web:6f35b0b8941b0af81a19da */
